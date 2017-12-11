7bf24d265a9097552507300cf3d2df135be59e35
Ivan Petrushev <ivan.petrushev@dnhsoft.com>
2017-12-11 11:47:01 +0200

    styles

--- webroot/js/scripts.js
+++ webroot/js/scripts.js
@@ -15,7 +15,7 @@ $(document).ready(function () {
     var mapboxLayer = L.tileLayer('https://{s}.tiles.mapbox.com/styles/v1/openstreetmap/cj8gojt0i1eau2rnn7q4mdgu7/tiles/256/{z}/{x}/{y}?access_token={access_token}', {
         subdomains: ['a', 'b', 'c', 'd'],
         access_token: 'pk.eyJ1Ijoib3BlbnN0cmVldG1hcCIsImEiOiJhNVlHd29ZIn0.ti6wATGDWOmCnCYen-Ip7Q'
-    }).addTo(map);
+    });
 
     var tileServerUrl = 'http://' + window.location.hostname + ':8011'
 
