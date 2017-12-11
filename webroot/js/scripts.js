650cff0abf303dfedb3e566240ab5a026b9b429f
Ivan Petrushev <ivan.petrushev@dnhsoft.com>
2017-12-11 09:40:34 +0200

    setting up webserver

---
+++ webroot/js/scripts.js
@@ -0,0 +1,14 @@
+
+$(document).ready(function () {
+
+    var map = L.map('mapid').setView([42.48112, 25.48645], 10);
+    L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>',
+        maxZoom: 18
+    }).addTo(map);
+
+    L.control.scale().addTo(map);
+
+    var hash = new L.Hash(map);
+
+});
